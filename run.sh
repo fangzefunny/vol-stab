3c13b22c68180c57e01a3625b3ebb503e4855c96
fangz5 <fangz5@rpi.edu>
2021-09-14 22:40:04 -0400

    add cores

--- run.sh
+++ run.sh
@@ -10,5 +10,5 @@ declare models=("model1" "model2" "model7"
 ## step 1: fit the model
 for model in "${models[@]}"; do 
     echo Model=$model
-        python m1_fits.py -n=$model -s=2415 -f=250
+        python m1_fits.py -n=$model -s=2415 -f=250 -c=50
 done 
