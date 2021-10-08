ca277079894e6398acd62c8c3c4191af15d904eb
zf <fangzefunn@gmail.com>
2021-10-08 15:24:04 -0400

    1

--- run.sh
+++ run.sh
@@ -5,13 +5,12 @@ python m0_preprocess.py
 
 ## declare all models and all data sets
 declare data_sets=("rew_data_exp1" "pain_data_exp1")
-declare models=("model1" "model2" "model7" 
-                "model8" "model11" "RRmodel1")
+declare models=("RRmodel")
 
 ## step 1: fit the model
 for data_set in "${data_sets[@]}"; do 
     for model in "${models[@]}"; do 
         echo Data set=$data_set Model=$model 
-            python m1_fits.py -d=$data_set -n=$model -s=215 -f=200 -c=100 -m='mle'
+            python m1_fits.py -d=$data_set -n=$model -s=22315 -f=200 -c=100 -m='mle'
     done 
 done
