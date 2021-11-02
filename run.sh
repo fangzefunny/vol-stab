c5f97094efdd35240c1c0c86c8cffba0122f4c73
Zeming <fangzefunny@gmail.com>
2021-11-01 23:33:18 -0400

    edit bash

--- run.sh
+++ run.sh
@@ -5,13 +5,16 @@ python m0_preprocess.py
 
 ## declare all models and all data sets
 declare data_sets=("rew_data_exp1")
-#declare models=("model1" "model2" "model11" "RRmodel")
-declare models=("model1"  "model2")
+declare groups=("ind" "avg")
+declare models=("model1" "model2" "model11" "RRmodel")
 
 ## step 1: fit the model
 for data_set in "${data_sets[@]}"; do 
-    for model in "${models[@]}"; do 
-        echo Data set=$data_set Model=$model 
-            python m1_fits.py -d=$data_set -n=$model -s=21123 -f=50 -c=50 -m='mle' -g='avg'
-    done 
+    for group in "${groups[@]}"; do 
+        for model in "${models[@]}"; do 
+            echo Data set=$data_set Group=$group Model=$model 
+                python m1_fits.py -d=$data_set -n=$model -s=21123 -f=100 -c=100 -m='mle' -g=$group
+                python m3_simulate.py -n=$model
+        done 
+    done
 done
