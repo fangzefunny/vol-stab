5d06f02020bb5b3d4b575388677c89a89081baa4
fangz5 <fangz5@rpi.edu>
2021-12-21 16:30:36 -0500

    e trace

--- run.sh
+++ run.sh
@@ -5,8 +5,8 @@ python m0_preprocess.py
 
 ## declare all models and all data sets
 declare data_sets=("rew_data_exp1")
-declare groups=("avg")
-declare models=("dual_sys")
+declare groups=("ind")
+declare models=("model11_e" "model11_m")
 #declare models=("model1" "model2" "model11" "RRmodel")
 
 ## step 1: fit the model
@@ -14,7 +14,7 @@ for data_set in "${data_sets[@]}"; do
     for group in "${groups[@]}"; do 
         for model in "${models[@]}"; do 
             echo Data set=$data_set Group=$group Model=$model 
-                python m1_fits.py -d=$data_set -n=$model -s=21123 -f=100 -c=100 -m='mle' -g=$group
+                python m1_fits.py -d=$data_set -n=$model -s=42 -f=100 -c=100 -m='mle' -g=$group
                 python m3_simulate.py -n=$model
         done 
     done
